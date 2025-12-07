de7de4856bb975be19ef0fa9e21f2206172be7d1
Faris-at-Home <90097027+rsuntk@users.noreply.github.com>
2025-12-07 13:33:46 +0700

    build: Add destdir

--- build.sh
+++ build.sh
@@ -26,6 +26,7 @@ KLEAF_ARGS=("${DEBUG_ARGS} ${SANDBOX_ARGS} \
 	--config=stamp --repo_manifest=${ROOT_DIR}/${DEVICE_MODULES_DIR}/fake_manifest.xml \
 	--experimental_writable_outputs \
 	--noenable_bzlmod \
+	--destdir=out \
 	--//build/bazel_mgk_rules:kernel_version=${KERNEL_VERSION_NUM}")
 
 set -x
